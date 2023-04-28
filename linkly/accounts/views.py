279b1054feae52a43e05579bc94a712a40f2906c
Davide Filosa <davideclaypool@gmail.com!>
2023-04-28 16:43:47 +0200

    created signup functionality

---
+++ linkly/accounts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignupForm
+# Create your views here.
+
+
+
+
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            authenticate(username=user.username, password=user.password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+    else:
+        form = SignupForm()
+        return render(request, 'accounts/signup.html', {'form': form})
--- linkly/app/urls.py
+++ linkly/app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index" )
+path("", views.index, name="index" ),
+path("about/", views.about, name="about" ),
+path("pricing/", views.pricing, name="pricing" ),
+
+
     
 ]

c863dbfda91ad0ee554d3e05f7734f30215a7081
Davide Filosa <davideclaypool@gmail.com!>
2023-04-29 08:52:42 +0200

    created models Link and Category

---
+++ linkly/link/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from accounts.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=225)
+    created_by = models.ForeignKey(User, related_name="categories", on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Link(models.Model):
+    category = models.ForeignKey(Category, related_name='links', on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    decription = models.TextField(null=True, blank=True)
+    url = models.CharField(max_length=225)
+    created_by = models.ForeignKey(User, related_name="links", on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
--- linkly/linkly/settings.py
+++ linkly/linkly/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'accounts'
+    'accounts',
+    'link'
 ]
 
 MIDDLEWARE = [

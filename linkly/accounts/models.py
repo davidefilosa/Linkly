119eb88f4dea0f4922b09daf3b1c1990c09e0ffe
Davide Filosa <davideclaypool@gmail.com!>
2023-04-29 15:02:58 +0200

    added Plans and limitation

--- linkly/accounts/models.py
+++ linkly/accounts/models.py
@@ -3,5 +3,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Plan(models.Model):
+    name = models.CharField(max_length=225)
+    max_num_link = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+
 class User(AbstractUser):
-    pass
+    plan = models.ForeignKey(Plan, related_name='user', on_delete=models.CASCADE, default=1)
+    
